cf7bfa090cc57fff07f58db71c5c5bb4dd814705
Jerzy Chalupski <jerzy@futuresimple.com>
2012-10-04 09:25:28 +0200

    Formatting

--- src/com/chalup/fontmetricstest/MainActivity.java
+++ src/com/chalup/fontmetricstest/MainActivity.java
@@ -9,16 +9,16 @@ import android.view.ViewGroup.LayoutParams;
 
 public class MainActivity extends Activity {
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        View fontMetricDemoVew = new View(this) {
-          @Override
-          protected void onDraw(Canvas canvas) {
-            super.onDraw(canvas);
-            canvas.drawColor(Color.RED);
-          }
-        };
-        setContentView(fontMetricDemoVew, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
-    }
+  @Override
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    View fontMetricDemoVew = new View(this) {
+      @Override
+      protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
+        canvas.drawColor(Color.RED);
+      }
+    };
+    setContentView(fontMetricDemoVew, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
+  }
 }
