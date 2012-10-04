2726b9e3f27d422e0dc6af93fe30db15d17ee094
Jerzy Chalupski <jerzy@futuresimple.com>
2012-10-04 09:24:16 +0200

    Removed unused layout

--- src/com/chalup/fontmetricstest/MainActivity.java
+++ src/com/chalup/fontmetricstest/MainActivity.java
@@ -1,13 +1,24 @@
 package com.chalup.fontmetricstest;
 
 import android.app.Activity;
+import android.graphics.Canvas;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup.LayoutParams;
 
 public class MainActivity extends Activity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        View fontMetricDemoVew = new View(this) {
+          @Override
+          protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            canvas.drawColor(Color.RED);
+          }
+        };
+        setContentView(fontMetricDemoVew, new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.MATCH_PARENT));
     }
 }
